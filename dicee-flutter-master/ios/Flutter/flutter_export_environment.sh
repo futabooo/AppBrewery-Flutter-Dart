2ca3c0cfde8db7e69a21f4ba97ce3f7a351ca7cd
futabooo <takahiro.futagawa@gmail.com>
2020-03-08 22:38:40 +0900

    add Dicee base

---
+++ dicee-flutter-master/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/latest/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/futabooo/dev/src/github.com/futabooo/AppBrewery-Flutter-Dart/dicee-flutter-master"
+export "FLUTTER_TARGET=/Users/futabooo/dev/src/github.com/futabooo/AppBrewery-Flutter-Dart/dicee-flutter-master/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/Caskroom/flutter/latest/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
