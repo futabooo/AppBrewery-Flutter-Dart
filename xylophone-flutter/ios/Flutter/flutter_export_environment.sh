ac51777ab2a28ea261a3e3d8a05fa07870c3679c
futabooo <takahiro.futagawa@gmail.com>
2021-01-09 17:32:35 +0900

    add xylophone-flutter initial project

---
+++ xylophone-flutter/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/usr/local/Cellar/flutter/sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/futabooo/dev/src/github.com/futabooo/AppBrewery-Flutter-Dart/xylophone-flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/Cellar/flutter/sdk/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
