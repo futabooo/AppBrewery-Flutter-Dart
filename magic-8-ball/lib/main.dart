fd127ea1d4412aada44ae5baa79a4a4d8ce969c4
futabooo <takahiro.futagawa@gmail.com>
2021-01-09 16:55:06 +0900

    create stateless widget

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
+      MaterialApp(home: BallPage()),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(),
+    );
+  }
+}
