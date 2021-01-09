0415d13670b0aa8fe1ce704f958d27a164c4a6b9
futabooo <takahiro.futagawa@gmail.com>
2021-01-09 12:36:39 +0900

    update

--- dicee-flutter-master/ios/Flutter/flutter_export_environment.sh
+++ dicee-flutter-master/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/latest/flutter"
+export "FLUTTER_ROOT=/usr/local/Cellar/flutter/sdk"
 export "FLUTTER_APPLICATION_PATH=/Users/futabooo/dev/src/github.com/futabooo/AppBrewery-Flutter-Dart/dicee-flutter-master"
 export "FLUTTER_TARGET=/Users/futabooo/dev/src/github.com/futabooo/AppBrewery-Flutter-Dart/dicee-flutter-master/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/usr/local/Caskroom/flutter/latest/flutter/bin/cache/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/Cellar/flutter/sdk/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
