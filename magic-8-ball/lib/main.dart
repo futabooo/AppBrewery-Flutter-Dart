90786cd8364d6f84517d88cd0f6a99a63c10a0dc
futabooo <takahiro.futagawa@gmail.com>
2021-01-09 17:17:21 +0900

    make it possible to change state

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -24,14 +25,18 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: FlatButton(
         onPressed: () {
-          print('I got clicked');
+          setState(() {
+            ballNumber = Random().nextInt(4) + 1;
+          });
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
