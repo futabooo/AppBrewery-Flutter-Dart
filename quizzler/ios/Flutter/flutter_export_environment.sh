91f538cfc7684622de5fd8110893831993c1af3e
futabooo <takahiro.futagawa@gmail.com>
2021-03-18 00:45:22 +0900

    fix

--- quizzler/ios/Flutter/flutter_export_environment.sh
+++ quizzler/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/Cellar/flutter/sdk"
+export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/2.0.1/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/futabooo/dev/src/github.com/futabooo/AppBrewery-Flutter-Dart/quizzler"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/usr/local/Cellar/flutter/sdk/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
